4714899134e8c2a77c4e72a482403d2d4543b01b
boyisboyis <boyisboyis261129@gmail.com>
2015-02-22 08:39:05 +0000

    Rewrite File connect DB

--- portal_access.php
+++ portal_access.php
@@ -7,6 +7,9 @@ if($GLOBALS["environment"]["maintain"] == 1){
 }
 
 $uri = ROUTE::getURI()->conPath();
+$db_config = parse_ini_file(__DIR__.'/config/db.ini');
+$DB = new DB($db_config['host'], $db_config['username'], ini_get('mysqli.default_pw'), $db_config['dbname']);
+$DB->connect();
 if($uri == ""){
   ROUTE::getPage("home");
 }
