dcac451b09b5110a74bf34c73e077d05b86366e0
boyisboyis <boyisboyis261129@gmail.com>
2015-02-22 08:18:01 +0000

    Adding Old File to Server

--- app/paths.php
+++ app/paths.php
@@ -2,10 +2,26 @@
 
 return array(
   
+  /* 
+  * Path file
+  */
   "home" => array(
-    "file" =>  "app/view/application/application.php"
-  )
+    "file" => "app/view/application/application.php",
+    "type" => "require"
+  ),
   
+  /*
+   *Javascript file
+  */
+  
+  "jquery" => array(
+      "file" => "lib/asset/js/jquery.min.js",
+      "type" => "file_get_contents"
+    ),
+  "jquery-ui" => array(
+      "file" => "lib/asset/js/jquery-ui.js",
+      "type" => "file_get_contents"
+    ) 
   
 );
 
--- portal_access.php
+++ portal_access.php
@@ -7,41 +7,11 @@ if($GLOBALS["environment"]["maintain"] == 1){
 }
 
 $uri = ROUTE::getURI()->conPath();
-print_r($uri);
-echo "test";
-echo "test2";
- // $result =  ROUTE::getURI();
- // echo $result;
- // echo "<br/>";
- // //echo Redirect::to();
- // if($result === "/"){
- //   Redirect::to("home");
- //   //require __DIR__.'/app/view/application/application.php';
- //   die();
- // }
- // else{
-   
- // }
+if($uri == ""){
+  ROUTE::getPage("home");
+}
+else{
+ ROUTE::getPage($uri);
+}
  
-  // $route = new ROUTE;
-  // echo $route->getPath();
- // echo ROUTE::getPath()->getArray();
-  
-  //$paths = require __DIR__.'/bootstrap/paths.php';
-
-  //$requested = $paths['public'].$uri;
- // echo "<br>";
-  //echo $requested;
-  
-  
- /* $config = parse_ini_file('config/db.ini');
-   $db = new DB($config['host'], $config['username'], ini_get('mysqli.default_pw'), $config['dbname']);
-   $db->connect();*/
-  /*$config = parse_ini_file('config/db.ini');
-  //$this->con = mysqli_connect($config['host'], $config['username'], ini_get('mysqli.default_pw'), $config['dbname']);
-  $GLOBALS["db_global"] = new DB($config['host'], $config['username'], "", $config['dbname']);
-  $GLOBALS["db_global"]->connect();*/
-  
-  
-
 ?>
