2371ebbda2f38f1a5de05840a3e6efb98ebb4577
boyisboyis <boyisboyis261129@gmail.com>
2015-02-26 08:38:16 +0000

    Require help/check.inc

--- lib/includes.php
+++ lib/includes.php
@@ -1,8 +1,12 @@
 <?php
   
   require 'model/db.inc';
+  
+  
   require 'controller/route.inc';
   require 'controller/redirect.inc';
   require 'controller/env.inc';
+  
+  require 'help/check.inc';
 
 ?>
