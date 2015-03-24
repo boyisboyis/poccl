a83dd88facd730c69ca005b54d8cd2d2c49487f4
Chonlawit <taro_rs@hotmail.com>
2015-03-24 16:17:02 +0000

    Fixed : chage => charge && Adding : Logic insert data

--- app/controller/admins_controller.php
+++ app/controller/admins_controller.php
@@ -16,12 +16,39 @@
     
     class adminController {
         public static function create($params) {
-            $sql = 'INSERT INTO `po_asso` (`JID`, `Contractor_Name`, `PO_No`) VALUES (' . self::nullValue($params['job_no']) . ', ' . self::nullValue($params['contractor_name']) . ', ' . self::nullValue($params['po_no']) . ')';
-            $result = DB::query($sql)->get();
+            
+            $sql_po_asso = 'INSERT INTO `po_asso` (`JID`, `Contractor_Name`, `PO_No`)' .
+                           'VALUES (' . self::nullValue($params['job_no']) . ', ' . 
+                                        self::nullValue($params['contractor_name']) . ', ' . 
+                                        self::nullValue($params['po_no']) . 
+                                        ')';
+            DB::query($sql_po_asso)->get();
+            
+            $sql_job = 'INSERT INTO `job` (`JID`, `Project_Name`, `Project_Location`, `Project_Owner`, `Secrecy_Agreement`, `Work_Start_Date`, `Work_Complete_Date`, `PO_Date`, `PO_Type`, `Contract_Value_THB`, `Contract_Value_Other`, `Contract_Value_Type`, `Contract_Value_Rate`, `Goveming_Law`, `Credit_Term` ,`Late_Pay_Finan_Charge`) ' .
+                       'VALUES (' . self::nullValue($params['job_no']) . ', ' .
+                                    self::nullValue($params['project_name']) . ', ' . 
+                                    self::nullValue($params['project_location']) . ', ' .
+                                    self::nullValue($params['project_owner_name']) . ', ' .
+                                    self::nullValue($params['secrecy_agreement']) . ', ' .
+                                    self::nullValue($params['start_date']) . ', ' .
+                                    self::nullValue($params['complete_date']) . ', ' .
+                                    self::nullValue($params['po_date']) . ', ' .
+                                    self::nullValue($params['po_type']) . ', ' .
+                                    self::nullValue($params['po_amount']) . ', ' .
+                                    self::nullValue($params['foreign_currency_value']) . ', ' .
+                                    self::nullValue($params['foreign_currency_type']) . ', ' .
+                                    self::nullValue($params['foreign_currency_rate']) . ', ' .
+                                    self::nullValue($params['goveming_law']) . ', ' .
+                                    self::nullValue($params['credit_term']) . ', ' .
+                                    self::nullValue($params['credit_term']) . ', ' .
+                                    self::nullValue($params['late_payment']) . ', ' .
+                                ')';
+             DB::query($sql_job)->get();
+            
             echo json_encode($sql);
         }
         public static function nullValue($str) {
-            if($str == 'none') {
+            if($str == 'none' || $str == '') {
                 return 'null';
             }
             else {
