245f85d0c679589f155e167a424850501c895bac
Chonlawit <taro_rs@hotmail.com>
2015-04-07 15:52:29 +0000

    Adding : Config Any Type

--- app/view/backends/_config_type.php
+++ app/view/backends/_config_type.php
@@ -1,14 +1,44 @@
-<div id="admin-add-user" class='t2'>
-    <div id='admin-add-user-block'>
+<div id="admin-config-type" class='t2'>
+    <div id='admin-config-type-block'>
         PO Type <a href="#config_type" id="add_po_type"><i class="fa fa-plus"></i></a>
+        <div id="add-po-type-alert" style="display: none;">
+    		<section>
+    			<h3>PO Type</h3>
+    			<input type="text" id="add-po-type-input" />
+    			<div>
+    				<button id="add-po-type-confirm-yes">Yes</button>
+    				<button id="add-po-type-confirm-no">NO</button>
+    			</div>
+    		</section>
+    	</div>
         <div id="result_po_type">
                 
         </div>
         Payment Type <a href="#config_type" id="add_payment_type"><i class="fa fa-plus"></i></a>
+        <div id="add-payment-type-alert" style="display: none;">
+    		<section>
+    			<h3>Payment Type</h3>
+    			<input type="text" id="add-payment-type-input" />
+    			<div>
+    				<button id="add-payment-type-confirm-yes">Yes</button>
+    				<button id="add-payment-type-confirm-no">NO</button>
+    			</div>
+    		</section>
+    	</div>
         <div id="result_payment_type">
                 
         </div>
         Guarantee Type <a href="#config_type" id="add_guarantee_type"><i class="fa fa-plus"></i></a>
+        <div id="add-guarantee-type-alert" style="display: none;">
+    		<section>
+    			<h3>Guarantee Type</h3>
+    			<input type="text" id="add-guarantee-type-input" />
+    			<div>
+    				<button id="add-guarantee-type-confirm-yes">Yes</button>
+    				<button id="add-guarantee-type-confirm-no">NO</button>
+    			</div>
+    		</section>
+    	</div>
         <div id="result_guarantee_type">
                 
         </div>
