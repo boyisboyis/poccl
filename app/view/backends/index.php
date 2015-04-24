a77428d372e8fef6174ad13f31db4513db64a830
boyisboyis <boyisboyis261129@gmail.com>
2015-04-24 17:22:17 +0000

    Update

--- app/view/backends/index.php
+++ app/view/backends/index.php
@@ -88,10 +88,10 @@
   </div>
   
 	<div id='alert-backup-db' style='display: none;'>
-		<section id="alert-backup-db-complete">
+		<section id="alert-backup-db-complete" style="text-align:center;">
 			<h3>Save Complete</h3>
-			<button id="alert-btn-backup-yes">Yes</button>
-			<button id="alert-btn-backup-no">No</button>
+			<button id="alert-btn-backup-yes" style="width: 30%;">Yes</button>
+			<button id="alert-btn-backup-no" style="width: 30%;">No</button>
 		</section>
 	</div>
 </body>
