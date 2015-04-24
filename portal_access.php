5b39d0f3d25dc175e7a9d7ea8b3d03cd7c038d3c
Chonlawit <taro_rs@hotmail.com>
2015-04-24 18:49:04 +0000

    Adding : Check ip logic

--- portal_access.php
+++ portal_access.php
@@ -1,8 +1,10 @@
 <?php
 session_start();
+
+  define('FIXEDIP', '223.204.249.97');
   ################### Check Mac Address Before Render Page ###################//
  
-  // $ipclient = get_client_ip();
+  $GLOBALS['IPCLIENT'] = get_client_ip();
   // $macAddr = false;
   // print_r($ipclient);
       
@@ -81,24 +83,24 @@ session_start();
   //   echo '</table>' ;
   // }
 
-  // function get_client_ip() {
-  //   $ipaddress = '';
-  //   if (getenv('HTTP_CLIENT_IP'))
-  //       $ipaddress = getenv('HTTP_CLIENT_IP');
-  //   else if(getenv('HTTP_X_FORWARDED_FOR'))
-  //       $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
-  //   else if(getenv('HTTP_X_FORWARDED'))
-  //       $ipaddress = getenv('HTTP_X_FORWARDED');
-  //   else if(getenv('HTTP_FORWARDED_FOR'))
-  //       $ipaddress = getenv('HTTP_FORWARDED_FOR');
-  //   else if(getenv('HTTP_FORWARDED'))
-  //     $ipaddress = getenv('HTTP_FORWARDED');
-  //   else if(getenv('REMOTE_ADDR'))
-  //       $ipaddress = getenv('REMOTE_ADDR');
-  //   else
-  //       $ipaddress = 'UNKNOWN';
-  //   return $ipaddress;
-  // }
+  function get_client_ip() {
+    $ipaddress = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if(getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    else if(getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if(getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if(getenv('HTTP_FORWARDED'))
+      $ipaddress = getenv('HTTP_FORWARDED');
+    else if(getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+  }
 
   // die();
     
