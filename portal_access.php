17d218e2d92c9e8fb3497db0c8616e495ef4ed7f
Chonlawit <taro_rs@hotmail.com>
2015-04-24 16:58:36 +0000

    Fixed : Get ip && Backup db with button

--- portal_access.php
+++ portal_access.php
@@ -1,15 +1,16 @@
 <?php
 session_start();
-  //################### Check Mac Address Before Render Page ###################//
+  ################### Check Mac Address Before Render Page ###################//
  
   // $ipclient = get_client_ip();
   // $macAddr = false;
+  // print_r($ipclient);
       
-  // #run the external command, break output into lines
+  #run the external command, break output into lines
   // $arp = `arp -a $ipclient`;
   // $lines = explode("\n", $arp);
       
-  // #look for the output line describing our IP address
+  #look for the output line describing our IP address
   // foreach($lines as $line) {
   //   $cols = preg_split('/\s+/', trim($line));
   //   print_r($cols);
@@ -18,8 +19,67 @@ session_start();
   //   }
   // }
   
-  // #This is MacAddress Client
+  #This is MacAddress Client
   // print_r($macAddr);
+  
+  // function get_client_ip() {
+  //   $indicesServer = array('PHP_SELF', 
+  //   'argv', 
+  //   'argc', 
+  //   'GATEWAY_INTERFACE', 
+  //   'SERVER_ADDR', 
+  //   'SERVER_NAME', 
+  //   'SERVER_SOFTWARE', 
+  //   'SERVER_PROTOCOL', 
+  //   'REQUEST_METHOD', 
+  //   'REQUEST_TIME', 
+  //   'REQUEST_TIME_FLOAT', 
+  //   'QUERY_STRING', 
+  //   'DOCUMENT_ROOT', 
+  //   'HTTP_ACCEPT', 
+  //   'HTTP_ACCEPT_CHARSET', 
+  //   'HTTP_ACCEPT_ENCODING', 
+  //   'HTTP_ACCEPT_LANGUAGE', 
+  //   'HTTP_CONNECTION', 
+  //   'HTTP_HOST', 
+  //   'HTTP_REFERER', 
+  //   'HTTP_USER_AGENT', 
+  //   'HTTPS', 
+  //   'REMOTE_ADDR', 
+  //   'REMOTE_HOST', 
+  //   'REMOTE_PORT', 
+  //   'REMOTE_USER', 
+  //   'REDIRECT_REMOTE_USER', 
+  //   'SCRIPT_FILENAME', 
+  //   'SERVER_ADMIN', 
+  //   'SERVER_PORT', 
+  //   'SERVER_SIGNATURE', 
+  //   'PATH_TRANSLATED', 
+  //   'SCRIPT_NAME', 
+  //   'REQUEST_URI', 
+  //   'PHP_AUTH_DIGEST', 
+  //   'PHP_AUTH_USER', 
+  //   'PHP_AUTH_PW', 
+  //   'AUTH_TYPE', 
+  //   'PATH_INFO', 
+  //   'ORIG_PATH_INFO',
+  //   'HTTP_CLIENT_IP',
+  //   'HTTP_X_FORWARDED_FOR',
+  //   'HTTP_X_FORWARDED',
+  //   'HTTP_FORWARDED_FOR',
+  //   'HTTP_FORWARDED') ; 
+    
+  //   echo '<table cellpadding="10">' ; 
+  //   foreach ($indicesServer as $arg) { 
+  //       if (isset($_SERVER[$arg])) { 
+  //           echo '<tr><td>'.$arg.'</td><td>' . $_SERVER[$arg] . '</td></tr>' ; 
+  //       } 
+  //       else { 
+  //           echo '<tr><td>'.$arg.'</td><td>-</td></tr>' ; 
+  //       } 
+  //   } 
+  //   echo '</table>' ;
+  // }
 
   // function get_client_ip() {
   //   $ipaddress = '';
